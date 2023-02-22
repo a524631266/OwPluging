5abccd3424498779e49a50000b691a666d2dc693
GitHub Action <action@github.com>
2023-02-22 13:37:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/a524631266/owpluging/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/a524631266/owpluging/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.a524631266.owpluging.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.a524631266.owpluging.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/a524631266/owpluging/services/MyProjectService.kt
+++ src/main/kotlin/com/github/a524631266/owpluging/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.a524631266.owpluging.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.a524631266.owpluging.MyBundle
 
 class MyProjectService(project: Project) {
 
